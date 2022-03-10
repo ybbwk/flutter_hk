3faaadc5eaedfa27ee036aaeb16292038c3e1d51
Leon Wong <ybbwk@vip.qq.com>
2022-03-10 11:01:42 +0800

    embedding v2 merge

--- example/android/app/src/main/java/jinhx/cn/flutter_hk_example/MainActivity.java
+++ example/android/app/src/main/java/jinhx/cn/flutter_hk_example/MainActivity.java
@@ -1,7 +1,7 @@
 package jinhx.cn.flutter_hk_example;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
